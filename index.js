ae6655eff94a0fdba731746830edf22efb85c43d
samie820 <samuelomole70@gmail.com>
2018-03-06 19:56:02 +0100

    Initial commit with basic CLI functionality

---
+++ index.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const Git = require('nodegit');
+const GitHub =  require('github-api');
+const axios = require('axios');
+const { prompt } = require('inquirer');
+
+API_KEY = '9798a0f185946a2365c04880fea3ced1a97296ab'
+
+const gh =new GitHub({
+    token: API_KEY
+})
+
+const questions = [
+    {
+      type : 'input',
+      name : 'repo',
+      message : 'Enter the type of repository you want ... e.g react, react-firebase etc.'
+    },
+    {
+      type : 'input',
+      name : 'language',
+      message : 'Enter language that the repo might use (this is just to streamline the search) ...'
+    },
+ 
+  ];
+
+program
+    .version('0.0.1')
+    .description('Installs the best boilerplate from github for you!!!')
+
+program
+    .command('start')
+    .alias('s')
+    .description('Create a new project from the boilerplate type')
+    .action(() => {
+        prompt(questions)
+        .then(answers => 
+            axios.get(`https://api.github.com/search/repositories?q=${answers.repo}+language:${answers.language}+in:name&sort=stars&order=desc/?access_token=${API_KEY}`)
+            .then((res) => {
+                const data = res.data.items
+                var git_data = data[0]
+                console.info(`Currently cloning and downloading ${git_data.git_url}, it has ${git_data.stargazers_count} stars and was last updated at ${git_data.updated_at}`)
+                return [git_data.git_url, answers]
+            })
+            .then((data) => {
+                console.info("Downloading the repo...")
+                Git.Clone(data[0], data[1].repo).then(() => {
+                    console.info('Your Repo is ready!!!')
+                })
+            })
+            .catch(err => console.info("Unable to get repo, please check your connection"))
+        )
+        .catch(err => console.info(err))
+    });
+
+
+program.parse(process.argv);
