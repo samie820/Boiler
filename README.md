3e5e44cfabfdf6a232fff1b0a71ab41d31583938
samie820 <samuelomole70@gmail.com>
2018-03-07 12:17:16 +0100

    Added it to npm repository

--- README.md
+++ README.md
@@ -1,18 +1,13 @@
 # Boiler
 A CLI tool for cloning the top rated repos, please i really that if you have any ideas to add to the project feel free to make a PR
 
-Project will soon be available on NPM for installation
-For now you can clone the repo.
+Run `npm install -g @samie820/boiler` to install boiler globally on your system
 
-After cloning, head over to github ad create your access token
-# Instructions:
+then, run `broiler list`, this will ask you some questions which include the search query.
 
-1. Go to settings.
-2. Go to developer settings
-3. In developer settings, select the personal access token option 
-4. Create a token and copy it
+# Warning: Make sure to run the `broiler list` command first so as to know the number which you would use in the next command
 
-When you have your token
+Then,run `broiler start` which will ask you for some other queries, including the number of the repo you want to clone as indicated in the previous command.
 
-Run the command `npm link`
-Then run `boiler init`
+
+
--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const Git = require('nodegit');
 const GitHub =  require('github-api');
 const axios = require('axios');
 const { prompt } = require('inquirer');
+const fs = require('fs');
+
+
 
 var API_KEY;
-
 const questions = [
     {
       type : 'input',
@@ -19,6 +21,32 @@ const questions = [
       name : 'language',
       message : 'Enter language that the repo might use (this is just to streamline the search) ...'
     },
+
+ 
+  ];
+
+  const select = [
+    {
+        type : 'input',
+        name : 'repo',
+        message : 'Enter the type of repository you want ... e.g react, react-firebase etc.'
+      },
+      {
+        type : 'input',
+        name : 'language',
+        message : 'Enter language that the repo might use (this is just to streamline the search) ...'
+      },  
+    {
+        type: 'input',
+        name: 'number',
+        message: 'Number of the repo, default (1):'
+
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Name of the folder you want to clone the repo into:'
+    }
  
   ];
 const creds = [
@@ -38,19 +66,38 @@ program
     .alias('s')
     .description('Create a new project from the boilerplate type')
     .action(() => {
-        prompt(questions)
+        prompt(select)
         .then(answers => 
             axios.get(`https://api.github.com/search/repositories?q=${answers.repo}+language:${answers.language}+in:name&sort=stars&order=desc/?access_token=${API_KEY}`)
             .then((res) => {
                 const data = res.data.items
-                var git_data = data[0]
+                if(answers.number > 0){
+                  var git_data = data[answers.number-1]
+                }
+                else{
+                    var git_data = data[0]
+                }
+                
                 console.info(`Currently cloning and downloading ${git_data.git_url}, it has ${git_data.stargazers_count} stars and was last updated at ${git_data.updated_at}`)
-                return [git_data.git_url, answers]
+                return [git_data, answers]
             })
             .then((data) => {
                 console.info("Downloading the repo...")
-                Git.Clone(data[0], data[1].repo).then(() => {
+                Git.Clone(data[0].git_url, data[1].name).then(() => {
                     console.info('Your Repo is ready!!!')
+                    let metadata = {
+                        repo_name: data[0].name,
+                        repo_url: data[0].git_url,
+                        repo_stars: data[0].stargazers_count,
+                        description: data[0].description,
+                        updated_at: data[0].updated_at
+
+                    }
+                    let repo = JSON.stringify(metadata, null, 2)
+                    fs.writeFile('boiler.json',repo, (err) => {
+                        if(err) throw err;
+                        console.info('Check the boiler.json file to see details of the repo downloaded')
+                    });
                 })
             })
             .catch(err => console.info("Unable to get repo, please check your connection"))
@@ -75,17 +122,20 @@ program
     .description('Show a list of the 5 top repos based on your search')
     .action(() => {
         prompt(questions)
-        .then(answers => 
+        .then(answers => {
+            ANSWER = answers
             axios.get(`https://api.github.com/search/repositories?q=${answers.repo}+language:${answers.language}+in:name&sort=stars&order=desc/?access_token=${API_KEY}`)
             .then((res) => {
                 const data = res.data.items
                 data.map((repo, idx) => {
                     console.info(`${idx+1}. Name:${repo.name} | Author: ${repo.full_name} | Stars: ${repo.stargazers_count/1000}K`)
                 })
+
             })
             .catch(err => console.info("Unable to get repo, please check your connection"))
-        )
+    })
         .catch(err => console.info(err))
-    });
+    
+});
 
 program.parse(process.argv);
